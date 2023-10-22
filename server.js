274d78cbdc5ac235f8a2a8b409a7960039c1ab23
devLateef <lateefolatayo@gmail.com>
2023-10-22 08:27:27 +0100

    Configured Eslint rules

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+
+const dotenv = require('dotenv');
+
+
+
 const path = require('path');
+
 const app = express();
 
 app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'courseForm.html'));
+    res.sendFile(path.resolve(__dirname, 'pages/courseForm.html'));
 })
 app.listen(3000, ()=>{
     console.log('App listening on port 3000');
