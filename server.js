9ca3e25ec18cf305e802ff2b83c0285a0556057a
devLateef <lateefolatayo@gmail.com>
2023-10-22 12:32:34 +0100

    Implemented template engine

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-
 const dotenv = require('dotenv');
+const ejs = require('ejs');
 
-
+dotenv.config();
 
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/courseForm.html'));
+    res.render('courseForm');
 })
 app.listen(3000, ()=>{
     console.log('App listening on port 3000');
