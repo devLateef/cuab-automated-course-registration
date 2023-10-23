2a071ff77a1d6414390078c84b4b77d31be07eaf
devLateef <lateefolatayo@gmail.com>
2023-10-23 12:12:53 +0100

    Structured css for the homepage

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
     res.render('courseForm');
+});
+
+app.get('/dashboard', (req, res)=>{
+    res.render('dashboard');
 })
+
+
 app.listen(3000, ()=>{
     console.log('App listening on port 3000');
 })
