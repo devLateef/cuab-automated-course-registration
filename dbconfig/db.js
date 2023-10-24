bc9efa7c9107a0695076f91508418c14fb3d51f6
Luqman Adeniyi <luqmanadeniyi97@gmail.com>
2023-10-24 17:44:23 +0100

    fixed the conflicts

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
 });
 
-module.exports = pool.promise();
+module.exports = pool;
