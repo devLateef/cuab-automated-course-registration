92463286e2ce8911389b69e9115e3e8eba11e442
Luqman Adeniyi <luqmanadeniyi97@gmail.com>
2023-10-25 17:16:48 +0100

    course registration feature added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const ejs = require('ejs');
 const stdRoutes = require('./routes/studentmanagerroute');
+const courseRegRoutes = require('./routes/courseregistrationroute');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 require('./dbconfig/db.js');
 
@@ -24,11 +26,19 @@ app.get('/coursereg', (req, res) => {
   res.render('courseForm');
 });
 
+// Student Route
 app.use(stdRoutes);
+
+// Course Registration Route
+app.use(courseRegRoutes);
 
 app.get('/coursemanager', (req, res) => {
   res.render('coursemanager');
 });
+
+// error handling middleware
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
