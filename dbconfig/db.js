eca39ef341fb558e6570f336e1eed35d21fc52ca
devLateef <lateefolatayo@gmail.com>
2023-10-27 16:40:00 +0100

    Changed how I handled error in my database configuration file

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -9,9 +9,9 @@ const pool = mysql.createConnection({
   password: process.env.DB_PASSWORD,
 });
 
-pool.connect((err)=>{
-  if(err) return err.message;
-  console.log('Connected to the Database Successfully');
-})
+pool.connect((err) => {
+  if (err) throw new Error('Can not connect to the database');
+  console.log(`Connected to the Database Successfully`);
+});
 
 module.exports = pool;
