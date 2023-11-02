4376dec3d74f0cae7c2c5cf68b06753fbae14330
devLateef <lateefolatayo@gmail.com>
2023-11-02 12:00:18 +0100

    Installed Express Session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const ejs = require('ejs');
+const expressSession = require('express-session');
 const stdRoutes = require('./routes/studentmanagerroute');
 const courseRoutes = require('./routes/coursesmanagerroute');
 const courseRegRoutes = require('./routes/courseregistrationroute');
@@ -16,13 +17,19 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+app.use(
+  expressSession({
+    secret: 'Coding is simple',
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 //Auth
-app.use(authRoutes)
+app.use(authRoutes);
 
 // Student Route
 app.use(stdRoutes);
@@ -30,8 +37,6 @@ app.use(stdRoutes);
 // Course Registration Route
 app.use(courseRegRoutes);
 app.use(courseRoutes);
-
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
